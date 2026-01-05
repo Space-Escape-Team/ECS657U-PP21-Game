912d2be3883c29a4500e94de072b5118fdff5a22
Xeeneetee <154092341+Xeeneetee@users.noreply.github.com>
2026-01-05 17:50:51 +0000

    forest stage (level 2) committed

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerData
 {
-    
-    public int health;
     public float[] position;
     public int times_completed;
 
     public PlayerData (Player player)
     {
-        health = player.health;
 
         position = new float[3];
         position[0] = player.transform.position.x;
