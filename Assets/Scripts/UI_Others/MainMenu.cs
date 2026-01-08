daac9e6d3f89771f0fd6e232bf968257c1ab8d1d
Shaan9890 <shaan.mh.shah@gmail.com>
2026-01-08 20:30:53 +0000

    Removed duplicate function

--- Assets/Scripts/UI_Others/MainMenu.cs
+++ Assets/Scripts/UI_Others/MainMenu.cs
@@ -10,13 +10,6 @@ public class MainMenu : MonoBehaviour // the methods for the main menu
         Debug.Log("START PLEASE!"); // throws the given text into the debugger if successful
         LevelLoader.Instance.LoadNextLevel(); // Loads the next scene, which should be the hub area.
     }
-
-    public void PlayGame ()
-    {
-        Debug.Log("START PLEASE!"); // throws the given text into the debugger if successful
-        LevelLoader.Instance.LoadNextLevel(); // Loads the next scene, which should be the hub area.
-    }
-
     public void QuitGame ()
     {
         Debug.Log("QUIT PLEASE!"); // throws the given text into the debugger if successful
